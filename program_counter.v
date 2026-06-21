43ad827ccbe0d6290a26fff31997f7661b32ae8f
Muhi05 <24106089@nec.edu.in>
2026-06-21 08:04:32 +0530

    Create program_counter.v

---
+++ program_counter.v
@@ -0,0 +1,10 @@
+module program_counter (
+    input clk, rst, stall,
+    input [31:0] pc_next,
+    output reg [31:0] pc
+);
+    always @(posedge clk or posedge rst) begin
+        if (rst)         pc <= 32'b0;
+        else if (!stall) pc <= pc_next;
+    end
+endmodule
